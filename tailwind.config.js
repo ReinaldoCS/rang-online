dd7166ef0a520f62d626c185167bf8f7b09dc0ca
Reinaldo Santos <reinaldocs@Reinaldos-MacBook-Air.local>
2022-10-24 20:11:27 -0300

    Configurando TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.tsx'],
+  theme: {
+    colors: {
+      white: '#FFFFFF',
+      'gray-500': '#41414C',
+      'gray-400': '#B7B7CC',
+      'gray-100': '#F0F2F5',
+      'orange-500': '#F1972C',
+      'green-500': '#36B336',
+      'yellow-500': '#FFB84D',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
