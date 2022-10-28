814a7561ae8d13584d5e028c68e920064686f720
Reinaldo Santos <reinaldocs@Reinaldos-MacBook-Air.local>
2022-10-28 20:06:37 -0300

    Criando componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './index.css';
 
+import { Footer } from './Components/Footer';
 import { Header } from './Components/Header';
 import { LunchesList } from './Components/LunchesList';
 
 export function App() {
   return (
-    <>
+    <div className="w-screen h-screen flex flex-col flex-1">
       <Header />
       <LunchesList />
-    </>
+      <Footer />
+    </div>
   );
 }
